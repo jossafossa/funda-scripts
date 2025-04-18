93ae66d6db12171d896e02ca8ee179d2c3af38c3
Joost Hobma <joost.hobma@simplicate.nl>
2025-04-18 21:28:15 +0200

    Auto-commit on save

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,33 @@ const ITEMS =
   "#FilterPanel + div > .flex:has(a[href*='/detail/koop/']) > * > *:nth-last-child(1)";
 const ITEMS_CONTAINER = "h2 a";
 
+const getStorage = () => {
+  const storage = localStorage.getItem("checker");
+  if (!storage) {
+    localStorage.setItem("checker", JSON.stringify({}));
+    return {};
+  }
+  return JSON.parse(storage);
+};
+const setStorage = (storage: Record<string, boolean>) => {
+  localStorage.setItem("checker", JSON.stringify(storage));
+};
+const getChecked = (id: string) => {
+  const storage = getStorage();
+  const checked = storage[id];
+  if (checked === undefined) {
+    storage[id] = false;
+    setStorage(storage);
+    return false;
+  }
+  return checked;
+};
+const setChecked = (id: string, checked: boolean) => {
+  const storage = getStorage();
+  storage[id] = checked;
+  setStorage(storage);
+};
+
 const getCheckbox = () => {
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
@@ -20,10 +47,18 @@ const insertCheckbox = (root: Element) => {
   const id = getId(root);
   console.log({ id });
   root.classList.add("checkerContainer");
-  // root.classList.add("checkerContainerLink");
+  root.classList.add("checkerContainerLink");
   const container = root.querySelector(ITEMS_CONTAINER);
   container?.classList.add("checkerContainerLink");
-  root?.append(getCheckbox());
+  const checkbox = getCheckbox();
+
+  checkbox.checked = getChecked(id);
+  checkbox.addEventListener("change", (e) => {
+    const checked = (e.target as HTMLInputElement).checked;
+    setChecked(id, checked);
+  });
+
+  root?.append(checkbox);
 };
 
 document.querySelectorAll(ITEMS).forEach(insertCheckbox);
